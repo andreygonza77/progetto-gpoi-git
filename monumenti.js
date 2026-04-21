090a553efe3c51617558c2e116d50858737d2c57
neri <calamai.neri@itismeucci.com>
2026-04-21 22:57:36 +0200

    aggiunti gli script

--- monumenti.js
+++ monumenti.js
@@ -1,3 +1,11 @@
 function aggiungiMonumentoArray(monumento) {
-//calamai
+const monumenti = [];
+
+function aggiungiMonumentoArray(monumento) {
+    monumenti.push(monumento);
+}
+
+function ottieniListaMonumenti() {
+    return monumenti;
+}
 }
--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 const map = L.map('map').setView([41.9028, 12.4964], 13);
-L.tileLayer('https://openstreetmap.org{z}/{x}/{y}.png', {
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
-L.marker([41.9028, 12.4964]).addTo(map)
-    .bindPopup('Roma Centro')
-    .openPopup();
+
+document.getElementById("btnStats").addEventListener("click", function () {
+
+    const container = document.getElementById("tabella-container");
+
+   
+    container.innerHTML = "";
+
+    const monumenti = ottieniListaMonumenti();
+
+    const table = document.createElement("table");
+
+    monumenti.forEach((m, index) => {
+        const row = document.createElement("tr");
+
+        const colIndex = document.createElement("td");
+        colIndex.textContent = index + 1;
+
+        const colLat = document.createElement("td");
+        colLat.textContent = m.lat;
+
+        const colLng = document.createElement("td");
+        colLng.textContent = m.lng;
+
+        row.appendChild(colIndex);
+        row.appendChild(colLat);
+        row.appendChild(colLng);
+
+        table.appendChild(row);
+    });
+
+    container.appendChild(table);
+});
